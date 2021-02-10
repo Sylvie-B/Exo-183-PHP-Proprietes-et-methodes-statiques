505d73ff60ba8851a5749fa0acf71fa0e9c6c058
Sylvie-B <unsolo@hotmail.fr>
2021-02-10 17:07:53 +0100

    creating class

---
+++ VOD.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class VOD
+{
+    private array $film = [
+        "The fifth element",
+        "Usual Suspects",
+        "The Bourne Identity",
+        "Benjamin Gates",
+        "John Wick"
+    ];
+
+    private float $price;
+
+}
